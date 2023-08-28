b5cb1f87e1d326f38ac43261035705e1d052556c
Eterna1Student <oleg-kozlov91@rambler.ru>
2023-08-28 18:10:10 +0400

    добавил скрипт для бургера

--- src/js/component/app.js
+++ src/js/component/app.js
@@ -12,3 +12,23 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
+(function () {
+  const burgerBtn = document.querySelector('.header__burger-btn');
+  burgerBtn.addEventListener ('click', () => {
+    burgerBtn.classList.toggle('header__burger-btn-active');
+  });
+}());  
+
+
+const myButton = document.getElementById('header__burger-btn');
+const myElement = document.getElementById('burger');
+
+myButton.addEventListener('click', function() {
+  if (myElement.style.display === 'flex') {
+    myElement.style.display = 'none';
+  } else {
+    myElement.style.display = 'flex';
+  }
+});
